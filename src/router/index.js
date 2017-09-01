d7cf4b460e9fed6a1e94bc7b2fe00475d842c7ac
Dustyn Tubbs <DustynTubbs@Gmail.com>
2017-09-01 10:49:07 -0500

    Boiler platin' some components

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Navigator from '@/components/Navigator'
+import QueryBuilder from '@/components/QueryBuilder'
 
 Vue.use(Router)
 
@@ -10,6 +12,16 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/navigator',
+      name: 'Navigator',
+      component: Navigator
+    },
+    {
+      path: '/queryBuilder',
+      name: 'QueryBuilder',
+      component: QueryBuilder
     }
   ]
 })
